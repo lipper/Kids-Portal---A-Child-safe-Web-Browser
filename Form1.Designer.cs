210e50ae0a4fa08f08c9d1a2f9730e7d77cedb54
Jae Allen R. Nuguid <jaeallen035@hotmail.com>
2017-03-29 23:44:08 +0800

    Development of core browser
    web browser only.

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Browser1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.navBar = new System.Windows.Forms.TextBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.browser = new System.Windows.Forms.WebBrowser();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.navBar, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.progressBar, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.browser, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.550482F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 92.83721F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1.612299F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(980, 478);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            this.tableLayoutPanel1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.tableLayoutPanel1_MouseDoubleClick);
+            // 
+            // navBar
+            // 
+            this.navBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.navBar.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.navBar.Location = new System.Drawing.Point(3, 3);
+            this.navBar.Name = "navBar";
+            this.navBar.Size = new System.Drawing.Size(974, 29);
+            this.navBar.TabIndex = 4;
+            this.navBar.Text = "http://google.com";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar.ForeColor = System.Drawing.Color.DarkRed;
+            this.progressBar.Location = new System.Drawing.Point(3, 472);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(974, 3);
+            this.progressBar.TabIndex = 5;
+            this.progressBar.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // browser
+            // 
+            this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browser.Location = new System.Drawing.Point(3, 29);
+            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(974, 437);
+            this.browser.TabIndex = 6;
+            this.browser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.browser_DocumentCompleted);
+            this.browser.ProgressChanged += new System.Windows.Forms.WebBrowserProgressChangedEventHandler(this.browser_ProgressChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 478);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Kids Portal - Alpha 1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox navBar;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.WebBrowser browser;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Browser1
+{
+    public partial class Form1 : Form
+    {
+        string homepage = "http://google.com";
+        public Form1()
+        {
+
+            InitializeComponent();
+        //    FormBorderStyle = FormBorderStyle.None;
+     //   WindowState = FormWindowState.Maximized;
+     navBar.KeyDown += new KeyEventHandler(tb_KeyDown);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            navBar.Enabled = false;
+            browser.Navigate(homepage);
+            settings.Show();
+            settings.Visible = false;
+
+        }
+
+        Form2 settings = new Form2();
+        
+        private void tb_KeyDown(object sender, KeyEventArgs e)
+        {
+        
+            if (e.KeyCode == Keys.Enter)
+            {
+                navBar.Enabled = false;
+                if (navBar.Text=="settings") {
+                    navBar.Text = "http://";
+
+
+                    settings.Visible = true;          
+                    navBar.Enabled = true;
+
+
+
+                }
+                else if (passURL(navBar.Text))
+                {
+                    browser.Navigate(navBar.Text);
+                }
+
+            }
+
+           
+        }
+
+        private Boolean passURL(String text)
+        {
+            if (!(text.Contains(".com") || text.Contains(".org") || text.Contains(".net"))|| text.Contains(' '))
+            {
+                if (text.Contains(' '))
+                {
+                    string newText = "";
+                    string[] words = navBar.Text.Split(' ');
+
+                    for (int x = 0; x < words.Length; x++)
+                    {
+                        newText += words[x] + '+';
+                    }
+                }
+
+
+                browser.Navigate("https://www.google.com.ph/search?q=" + navBar.Text);
+                return false;
+            }
+            return true;
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            navBar.Enabled = true;
+            navBar.Text = e.Url.ToString();
+            if (browser.DocumentText.Contains("sex"))
+            {
+                MessageBox.Show("Inappropriate Page! - sex");
+            }
+        }
+
+        private void browser_ProgressChanged(object sender, WebBrowserProgressChangedEventArgs e)
+        {
+            if (e.CurrentProgress > 0 && e.CurrentProgress > 0)
+            {
+
+              
+                try
+                {
+                    int v = (int)(e.CurrentProgress * 100 / e.MaximumProgress);
+                    if (v > 100) v = 100;
+
+                    progressBar.Value = v;
+                }
+                catch
+                {
+                   
+                }
+               
+            }
+        }
+
+        private void tableLayoutPanel1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            
+            settings.Visible = true;
+
+
+        }
+    }
+}
